f561698a392cebb07656f9335f19c1cde85c40d9
Vinod-Ghanchi <80514865+Vinod-Ghanchi@users.noreply.github.com>
2022-04-09 13:07:36 +0530

    Update webscrapping.py

--- webscrapping.py
+++ webscrapping.py
@@ -8,7 +8,7 @@ import seaborn as sns
 import sys 
 webdriver_path = 'C:/Users/sidha/Downloads/chromedriver_win32/chromedriver.exe' # Enter the file directory of the Chromedriver
 chroma_url = 'https://www.croma.com/'
-search_item = 'IQOO Z5 5G' # Chose this because I often search for phones!
+search_item = 'OnePlus Nord CE 2 5G' # Chose this because I often search for phones!
 options = webdriver.ChromeOptions()
 options.add_argument('--headless') 
 options.add_argument('start-maximized') 
