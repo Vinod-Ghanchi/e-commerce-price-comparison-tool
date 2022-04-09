82b13ebc2a61909d8d86f9aa22b2332bab1f88c7
Vinod-Ghanchi <80514865+Vinod-Ghanchi@users.noreply.github.com>
2022-04-09 15:31:44 +0530

    Update webscrapping.py

--- webscrapping.py
+++ webscrapping.py
@@ -8,7 +8,8 @@ import seaborn as sns
 import sys 
 webdriver_path = 'D:/chromedriver_win32/chromedriver.exe' # Enter the file directory of the Chromedriver
 amazon_url = 'https://www.amazon.in/'
-search_item = 'OnePlus Nord CE 2 5G' # Chose this because I often search for coffee!
+search_item = 'OnePlus Nord CE 2 5G' 
+! pip install webdriver_manager
 # Select custom Chrome options
 from selenium.webdriver.common.by import By
 options = webdriver.ChromeOptions()
@@ -19,3 +20,7 @@ options.add_argument('--disable-extensions')
 # Open the Chrome browser
 browser = webdriver.Chrome(webdriver_path, options=options)
 browser.get(amazon_url)
+from selenium.webdriver.common.by import By
+search_bar = browser.find_element(By.ID,"twotabsearchtextbox")
+search_bar.send_keys(search_item)
+search_bar.submit
