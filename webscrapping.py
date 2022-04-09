f38e7577cad8dffad5144595e0e47042e9886ac7
sidharthvnair02 <82643868+sidharthvnair02@users.noreply.github.com>
2022-04-09 12:59:36 +0530

    Update webscrapping.py

--- webscrapping.py
+++ webscrapping.py
@@ -9,3 +9,11 @@ import sys
 webdriver_path = 'C:/Users/sidha/Downloads/chromedriver_win32/chromedriver.exe' # Enter the file directory of the Chromedriver
 flipkart_url = 'https://www.flipkart.com/'
 search_item = 'IQOO Z5 5G' # Chose this because I often search for phones!
+options = webdriver.ChromeOptions()
+options.add_argument('--headless') 
+options.add_argument('start-maximized') 
+options.add_argument('disable-infobars')
+options.add_argument('--disable-extensions')
+# Open the Chrome browser
+browser = webdriver.Chrome(webdriver_path, options=options)
+browser.get(flipkart_url)
