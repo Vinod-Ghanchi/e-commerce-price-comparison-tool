af8631b6fc3e3ae3eb9374c0113e6f515f0be9d7
sidharthvnair02 <82643868+sidharthvnair02@users.noreply.github.com>
2022-04-09 13:05:09 +0530

    Update webscrapping.py

--- webscrapping.py
+++ webscrapping.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import sys 
 webdriver_path = 'C:/Users/sidha/Downloads/chromedriver_win32/chromedriver.exe' # Enter the file directory of the Chromedriver
-flipkart_url = 'https://www.flipkart.com/'
+chroma_url = 'https://www.croma.com/'
 search_item = 'IQOO Z5 5G' # Chose this because I often search for phones!
 options = webdriver.ChromeOptions()
 options.add_argument('--headless') 
@@ -16,4 +16,4 @@ options.add_argument('disable-infobars')
 options.add_argument('--disable-extensions')
 # Open the Chrome browser
 browser = webdriver.Chrome(webdriver_path, options=options)
-browser.get(flipkart_url)
+browser.get(chroma_url)
